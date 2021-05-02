fe0ef6993cb31d66de013be393503eb16c0015e9
Keye Li <kli32@stevens.edu>
2021-05-02 01:16:04 -0400

    grading part completed 99.99999999%

---
+++ Quizapp/app/src/main/java/com/example/quizapp/Creator_GradeMatchingQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Creator_GradeMatchingQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creator__grade_matching_questions);
+    }
+}
